634cffe0f821d4ce6548fea941cfbae8f79d24dc
Aldin Kovačević <aldin@salesseek.com>
2023-10-12 04:57:59 +0200

    Path fix

--- src/main/java/ba/edu/ibu/library/api/impl/mailsender/SendgridSender.java
+++ src/main/java/ba/edu/ibu/library/api/impl/mailsender/SendgridSender.java
@@ -1,4 +1,4 @@
-package ba.edu.ibu.library.api.impl;
+package ba.edu.ibu.library.api.impl.mailsender;
 
 import ba.edu.ibu.library.core.api.mailsender.MailSender;
 import ba.edu.ibu.library.core.model.User;
@@ -9,14 +9,14 @@ import java.util.List;
 
 @Component
 // Method 2 for injection: condition based on application properties
-// @ConditionalOnProperty(name = "configuration.mailsender.default", havingValue = "mailgun")
-public class MailgunSender implements MailSender {
+// @ConditionalOnProperty(name = "configuration.mailsender.default", havingValue = "sendgrid")
+public class SendgridSender implements MailSender {
 
     @Override
     public String send(List<User> users, String message) {
         for (User user: users) {
             System.out.println("Message sent to: " + user.getEmail());
         }
-        return "Message: " + message + " | Sent via Mailgun.";
+        return "Message: " + message + " | Sent via Sendgrid.";
     }
 }
